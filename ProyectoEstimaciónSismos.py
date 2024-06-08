97f202c3ec1a1e71544aca23f85fd3518b25150d
Tatiana Noemi Estrada Carmen <tatiestra2012@gmail.com>
2024-06-08 11:40:07 -0500

    feat: Convertir columnas a tipo numerico

--- ProyectoEstimaciónSismos.py
+++ ProyectoEstimaciónSismos.py
@@ -1,1 +1,6 @@
-print Hola
+df['LATITUD'] = df['LATITUD'].str.replace(',', '.').astype(float)
+df['LONGITUD'] = df['LONGITUD'].str.replace(',', '.').astype(float)
+df['MAGNITUD'] = df['MAGNITUD'].str.replace(',', '.').astype(float)
+
+# Verificar que los datos ahora estén en formato numérico
+print(df.dtypes)
